244f5fa04e8f52169e88b5791c54643c2a0556b1
Viktor Yukhno <pingvinomania@gmail.com>
2021-10-27 16:34:44 +0300

    Initial commit

---
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package mate.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL =
+            "jdbc:mysql://remotemysql.com:3306/fuwU9gsZlw?serverTimezone=Europe/Istanbul";
+    private static final String USERNAME = "fuwU9gsZlw";
+    private static final String PASSWORD = "ZmvtnDHM2t";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
+    }
+}
